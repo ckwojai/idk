80e8ce5514162b7eb58721091a2f7c3543b7f418
Wilson Chang <ckwojai@gmail.com>
2017-10-09 03:11:14 -0700

    last try babe let's sleepppp Zzzzzzz

---
+++ pk.py
@@ -0,0 +1,4 @@
+i don't knwo what i m suppsoed to say but I can just figure it out and hope and pray
+i say her my name and says it's nice to meet ya then she handed me a cup of water, filled with tequila
+
+
